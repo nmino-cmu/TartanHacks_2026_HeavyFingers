8fafd139ea70ed9a9fdc6bc562ce8cdff27cdc51
hellothere98765 <64290251+hellothere98765@users.noreply.github.com>
2026-02-06 21:42:29 -0500

    Fixed up every_donate

--- every_donate.py
+++ every_donate.py
@@ -17,22 +17,58 @@ def get_donate_address(nonprofit_ein, amount, name, email):
     } 
 
     link = "https://api.givepact.io/v1/donate"
-    headers ={"Content-Type": "application/json"}
-    response = requests.post(link, headers=headers, data=payload)
+    header ={"Content-Type": "application/json"}
+    response = requests.post(link, headers=header, json=payload)
 
-    if(response.status != 200): return None
+    if(response.status_code != 200): print(response.status_code)
+
+    #response_json = response.json()
+
+    #deposit_addr = response_json["address"]
+
+
+    """debug_link = f"https://api.givepact.io/v1/verify?address={deposit_addr}"
+    debug_response = requests.get(debug_link)
+    if(debug_response.status !=200): return None
+    debug_response_json = debug_response.json()
+    print(debug_response_json)"""
+
+    #return deposit_addr 
+
+charities = {"earthday.org": "133798288", 
+             "Pacific Environmental Coalition":"843138671",
+             "Environmental Defense Fund":"116107128"}
+
+
+def get_donate_address(nonprofit_ein, amount, name, email):
+    payload = {
+        "ein": charities[nonprofit_ein],
+        "asset": "XRP",
+        "network": "Ripple",
+        "share_data": True,
+        "donor_name": name,
+        "donor_email": email
+    } 
+
+    link = "https://api.givepact.io/v1/donate"
+    header ={"Content-Type": "application/json"}
+    response = requests.post(link, headers=header, data=json.dumps(payload))
+
+    if(response.status_code != 200): print(response.status_code)
 
     response_json = response.json()
 
     deposit_addr = response_json["address"]
 
 
-    debug_link = f"https://api.givepact.io/v1/verify?address={deposit_addr}"
+    """debug_link = f"https://api.givepact.io/v1/verify?address={deposit_addr}"
     debug_response = requests.get(debug_link)
     if(debug_response.status !=200): return None
     debug_response_json = debug_response.json()
-    print(debug_response_json)
+    print(debug_response_json)"""
 
     return deposit_addr 
 
 
+m=get_donate_address("earthday.org", 10, "Steve", "e@e.com")
+print(m)
